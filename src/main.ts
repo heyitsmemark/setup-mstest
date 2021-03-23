fa40ebe73e8b64e8dcfa338089589c22e77afc8c
Mark <Mark.Elliott@globalknowledge.net>
2021-03-23 14:57:42 +0000

    test

---
+++ src/main.ts
@@ -0,0 +1,112 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as io from '@actions/io'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
+
+const IS_WINDOWS = process.platform === 'win32'
+const VS_VERSION = core.getInput('vs-version') || 'latest'
+const VSWHERE_PATH = core.getInput('vswhere-path')
+
+// if a specific version of VS is requested
+let VSWHERE_EXEC = '-products * -requires Microsoft.Component.mstest -property installationPath -latest '
+if (VS_VERSION !== 'latest') {
+  VSWHERE_EXEC += `-version "${VS_VERSION}" `
+}
+
+core.debug(`Execution arguments: ${VSWHERE_EXEC}`)
+
+async function run(): Promise<void> {
+  try {
+    // exit if non Windows runner
+    if (IS_WINDOWS === false) {
+      core.setFailed('setup-mstest can only be run on Windows runners')
+      return
+    }
+
+    // check to see if we are using a specific path for vswhere
+    let vswhereToolExe = ''
+
+    if (VSWHERE_PATH) {
+      // specified a path for vswhere, use it
+      core.debug(`Using given vswhere-path: ${VSWHERE_PATH}`)
+      vswhereToolExe = path.join(VSWHERE_PATH, 'vswhere.exe')
+    } else {
+      // check in PATH to see if it is there
+      try {
+        const vsWhereInPath: string = await io.which('vswhere', true)
+        core.debug(`Found tool in PATH: ${vsWhereInPath}`)
+        vswhereToolExe = vsWhereInPath
+      } catch {
+        // fall back to VS-installed path
+        vswhereToolExe = path.join(
+          process.env['ProgramFiles(x86)'] as string,
+          'Microsoft Visual Studio\\Installer\\vswhere.exe'
+        )
+        core.debug(`Trying Visual Studio-installed path: ${vswhereToolExe}`)
+      }
+    }
+
+    if (!fs.existsSync(vswhereToolExe)) {
+      core.setFailed(
+        'setup-mstest requires the path to where vswhere.exe exists'
+      )
+
+      return
+    }
+
+    core.debug(`Full tool exe: ${vswhereToolExe}`)
+
+    let foundToolPath = ''
+    const options: ExecOptions = {}
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        const installationPath = data.toString().trim()
+        core.debug(`Found installation path: ${installationPath}`)
+
+        let toolPath = path.join(
+          installationPath,
+          'Common7\\IDE\\MSTest.exe'
+        )
+
+        core.debug(`Checking for path: ${toolPath}`)
+        if (!fs.existsSync(toolPath)) {
+          toolPath = path.join(
+            installationPath,
+            'Common7\\IDE\\MSTest.exe'
+          )
+
+          core.debug(`Checking for path: ${toolPath}`)
+          if (!fs.existsSync(toolPath)) {
+            return
+          }
+        }
+
+        foundToolPath = toolPath
+      }
+    }
+
+    // execute the find putting the result of the command in the options foundToolPath
+    await exec.exec(`"${vswhereToolExe}" ${VSWHERE_EXEC}`, [], options)
+
+    if (!foundToolPath) {
+      core.setFailed('Unable to find mstest.')
+      return
+    }
+
+    // extract the folder location for the tool
+    const toolFolderPath = path.dirname(foundToolPath)
+
+    // set the outputs for the action to the folder path of mstest
+    core.setOutput('mstestPath', toolFolderPath)
+
+    // add tool path to PATH
+    core.addPath(toolFolderPath)
+    core.debug(`Tool path added to PATH: ${toolFolderPath}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
